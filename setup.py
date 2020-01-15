114cc36de8d36a767cdd6edb6b9fb51049c771cc
Marcos Aguayo <marcos@aguayo.es>
2020-01-15 11:25:52 +0100

    Download link

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def main():
         author = 'Marcos Aguayo',
         author_email = 'marcos@aguayo.es',
         url = 'https://github.com/maguayo/python-workers-kv',
-        download_url = 'https://github.com/maguayo/python-workers-kv',
+        download_url = 'https://github.com/maguayo/python-workers-kv/archive/master.zip',
         keywords = ['cloudflare', 'workers', 'kv', 'workers kv'],
         install_requires=['asn1crypto', 'certifi', 'cffi', 'chardet', 'cryptography', 'idna', 'pycparser', 'pyOpenSSL', 'requests'],
         classifiers=[
